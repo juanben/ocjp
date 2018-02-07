d48e7b2c567885705f00108de29b10e356fbcf28
Juan <juan.benavides@epn.edu.ec>
2018-02-07 16:17:00 -0500

    final

--- OcpjProj/src/ocpjproj/Collections.java
+++ OcpjProj/src/ocpjproj/Collections.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package collections;
+package ocpjproj;
 
 import java.util.Arrays;
 import java.util.List;
